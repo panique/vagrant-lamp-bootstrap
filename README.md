9e311f3fbfbdd19fb8180b26e2deaefe4e352ffb
Panique <panique@web.de>
2014-12-02 04:01:27 +0100

    boom!

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-vagrant-lamp-bootstrap
-======================
+# vagrant-lamp-bootstrap
 
-A super-simple Vagrantfile / bootstrap.sh to setup a LAMP stack inside Vagrant 100% automatically
+A super-simple Vagrantfile / bootstrap.sh to setup a LAMP stack inside Vagrant 100% automatically.
+
+### Whaaaaat ?
+
+This is a reduced-to-the-max Vagrant setup file, that will:
+
+1.) setup a Ubuntu 14.04 LTS "Trustry Thar" 64bit box
+2.) make the box accessable by the host at IP `192.168.33.22`
+3.) sync the current folder with `/var/www/html` inside the box
+4.) automatically perform all the commands in bootstrap.sh directly after setting up the box for the first time
+
+The bootstrap.sh will:
+
+5.) update, upgrade
+6.) create a folder inside /var/www/html
+7.) install apache 2.4, php 5.5, MySQL, PHPMyAdmin, git and Composer
+8.) also setting a pre-chosen password for MySQL and PHPMyAdmin
+7.) activate mod_rewrite and add *AllowOverride All* to the vhost settings
+
+You can folder and password inside the bootstrap.sh for sure.
+
+### How to use ?
+
+Put `Vagrantfile` and `bootstrap.sh` inside a folder and do a `vagrant up` on the command line.
+This box uses Ubuntu 14.04 LTS "Trustry Thar" 64bit, so if you don't have the basic box already, do a 
+`vagrant box add ubuntu/trusty64` before.
+
+### Why ?
+
+This is just my personal time-saving bootstrap for Vagrant, it might be useful for you too.
---
+++ bootstrap.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='12345678'
+PROJECTFOLDER='myproject'
+
+# create project folder
+sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECTFOLDER}"
+    <Directory "/var/www/html/${PROJECTFOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
